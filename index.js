df3487cc7aeade328fbfbb4271d8c472124c1c8a
Tarik AIT M'BAREK <taitmbarek@gmail.com>
2022-08-24 13:24:33 +0100

    implement open card on file switcher on sample app

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { NativeModules } from 'react-native';
 
 const { Knotapi } = NativeModules;
 
-const createSession = async () => {
-    return Knotapi?.createSession();
+const open = async (sessionId) => {
+    return Knotapi?.openCardSwitcher(sessionId);
 }
 
-export {createSession}
+export {open}
 export default Knotapi;
--- ios/Knotapi.m
+++ ios/Knotapi.m
@@ -1,38 +1,39 @@
 // Knotapi.m
 
 #import "Knotapi.h"
-#import "CardOnFileSwitcher-Swift.h"
+#import <React/RCTConvert.h>
+@import CardOnFileSwitcher;
 
+@interface Knotapi ()
+@property (nonatomic, strong) UIViewController* presentingViewController;
+@end
 
 @implementation Knotapi
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(createSession:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(openCardSwitcher:(NSString *)sessionId)
 {
-    CardOnFileSwitcherSession * session = [CardOnFileSwitcherSession new];
-        [session createSessionWithCompletionHandler:^(NSString *sessionId) {
-            NSLog(@"this is session id: %@", sessionId);
-            dispatch_async(dispatch_get_main_queue(), ^{
-                PasswordChangerViewController *vc = [[PasswordChangerViewController alloc] initWithSessionId:sessionId];
-                [vc setOnSuccessOnSuccess:^(NSString * merchant) {
-                    NSLog(@"this is error: %@", merchant);
-                }];
-                [vc setOnErrorOnError:^(NSString *error, NSString *message) {
-                    NSLog(@"this is error: %@", error);
-                    NSLog(@"this is message: %@", message);
-                }];
-                [vc setOnEventOnEvent:^(NSString *event, NSString *message) {
-                    NSLog(@"this is event: %@", event);
-                    NSLog(@"this is message: %@", message);
-                }];
-                [vc setOnExitOnExit:^{
-                    NSLog(@"this is exit");
-                }];
-                [self presentViewController:vc animated:NO completion:nil];
-
-            });
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.presentingViewController = RCTPresentedViewController();
+        CardOnFileSwitcherViewController *vc = [[CardOnFileSwitcherViewController alloc] initWithSessionId:sessionId];
+        [vc setOnSuccessOnSuccess:^(NSString * merchant) {
+            NSLog(@"this is error: %@", merchant);
         }];
+        [vc setOnErrorOnError:^(NSString *error, NSString *message) {
+            NSLog(@"this is error: %@", error);
+            NSLog(@"this is message: %@", message);
+        }];
+        [vc setOnEventOnEvent:^(NSString *event, NSString *message) {
+            NSLog(@"this is event: %@", event);
+            NSLog(@"this is message: %@", message);
+        }];
+        [vc setOnExitOnExit:^{
+            NSLog(@"this is exit");
+        }];
+        [self.presentingViewController presentViewController:vc animated:NO completion:nil];
+
+    });
 }
 
 @end
