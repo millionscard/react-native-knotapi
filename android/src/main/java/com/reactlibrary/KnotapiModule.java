fb3291b87dc9fe6855717e5393b2bde3988febe8
hebamoustafa <eng.heba4@gmail.com>
2022-09-07 00:30:10 +0200

    update android sdk

--- android/src/main/java/com/reactlibrary/KnotapiModule.java
+++ android/src/main/java/com/reactlibrary/KnotapiModule.java
@@ -3,7 +3,6 @@
 package com.reactlibrary;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import android.content.Intent;
 import android.app.Activity;
 import android.content.Context;
@@ -13,19 +12,17 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.BaseActivityEventListener;
 
 import com.knotapi.cardonfileswitcher.CardOnFileSwitcher;
 import com.knotapi.cardonfileswitcher.OnSessionEventListener;
+import com.knotapi.cardonfileswitcher.model.Customization;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import org.json.JSONArray;
-import org.json.JSONException;
-import java.lang.reflect.Type;
-import java.util.List;
+import java.util.ArrayList;
 
 public class KnotapiModule extends ReactContextBaseJavaModule {
 
@@ -84,21 +81,21 @@ public class KnotapiModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void openCardSwitcher(String sessionId, String merchants, Boolean isCancel, String customization) {
-        try {
-            cardOnFileSwitcher = CardOnFileSwitcher.getInstance();
-            cardOnFileSwitcher.init(context, onSessionEventListener, customization);
-            // convert string merchants to array of merchants
-            Gson converter = new Gson();
-            Type type = new TypeToken<List<Integer>>(){}.getType();
-            JSONArray jsonArray = new JSONArray(merchants);
-            List<Integer> list =  converter.fromJson(jsonArray.toString(), type);
-            int[] merchantsArr = list.stream().mapToInt(Integer::intValue).toArray();
-            //
-            cardOnFileSwitcher.openCardOnFileSwitcher(sessionId, merchantsArr, isCancel);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+    public void openCardSwitcher(String sessionId, ReadableArray merchants, Boolean isCancel, ReadableMap customization) {
+      // handle customization object
+      Customization customizationObj = new Customization();
+      customizationObj.setPrimaryColor(customization.getString("primaryColor"));
+      customizationObj.setTextColor(customization.getString("textColor"));
+      customizationObj.setCompanyName(customization.getString("companyName"));
+      // convert ReadableArray merchants to array of int
+      ArrayList<Integer> arrayList = new ArrayList<>();
+      for (int i = 0; i <  merchants.size(); i++) {
+        int item = merchants.getInt(i);
+        arrayList.add(item);
+      }
+      int[] merchantsArr = arrayList.stream().mapToInt(Integer::intValue).toArray();
+      cardOnFileSwitcher = CardOnFileSwitcher.getInstance();
+      cardOnFileSwitcher.init(context, onSessionEventListener, customizationObj);
+      cardOnFileSwitcher.openCardOnFileSwitcher(sessionId, merchantsArr, isCancel);
     }
-
 }
