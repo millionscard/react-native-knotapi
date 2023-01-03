1dc81229154617ec43410c29ab1f7025de68cfa4
Tarik AIT M'BAREK <taitmbarek@gmail.com>
2023-01-03 17:54:27 +0100

    fix development env issue

--- ios/Knotapi.mm
+++ ios/Knotapi.mm
@@ -27,6 +27,9 @@ RCT_EXPORT_METHOD(openCardSwitcher:(NSDictionary *)params){
       if ([environmentString isEqualToString:@"sandbox"]) {
           environment = EnvironmentSandbox;
       }
+      if ([environmentString isEqualToString:@"development"]) {
+          environment = EnvironmentDevelopment;
+      }
       CardOnFileSwitcherSession *session = [[CardOnFileSwitcherSession alloc] initWithSessionId:sessionId clientId:clientId environment:environment];
       CardSwitcherConfiguration *config = [[CardSwitcherConfiguration alloc] init];
       [config setOnSuccessOnSuccess:^(NSString *merchant) {
@@ -66,6 +69,9 @@ RCT_EXPORT_METHOD(openSubscriptionCanceler:(NSDictionary *)params){
       Environment environment = EnvironmentProduction;
       if ([environmentString isEqualToString:@"sandbox"]) {
           environment = EnvironmentSandbox;
+      }
+      if ([environmentString isEqualToString:@"development"]) {
+          environment = EnvironmentDevelopment;
       }
 
       bool amount = true;
