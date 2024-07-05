1dd8a1450e15c8f8e7f34ed2f492ab36f35fb09d
hebamoustafa <eng.heba4@gmail.com>
2024-07-06 02:33:18 +0300

    Update Knotapi.mm

--- ios/Knotapi.mm
+++ ios/Knotapi.mm
@@ -83,12 +83,6 @@ RCT_EXPORT_METHOD(updateCardSwitcherSessionId:(NSString *)sessionId){
 
 RCT_EXPORT_METHOD(openSubscriptionManager:(NSDictionary *)params){
   dispatch_async(dispatch_get_main_queue(), ^{
-      NSString *companyName = nil;
-      NSString *textColor = nil;
-      NSString *primaryColor = nil;
-      NSNumber *buttonCorners = @(0.0);
-      NSNumber *buttonFontSize = @(0.0);
-      NSNumber *buttonPaddings = @(0.0);
       NSString *sessionId = [params objectForKey:@"sessionId"];
       NSString *clientId = [params objectForKey:@"clientId"];
       NSArray<NSNumber*> *merchantIds = [params objectForKey:@"merchantIds"];
@@ -97,82 +91,58 @@ RCT_EXPORT_METHOD(openSubscriptionManager:(NSDictionary *)params){
       NSString *entryPoint = [params objectForKey:@"entryPoint"];
       BOOL useCategories = [[params objectForKey:@"useCategories"] boolValue];
       BOOL useSearch = [[params objectForKey:@"useSearch"] boolValue];
-      BOOL useSelection = [[params objectForKey:@"useSelection"] boolValue];
-      BOOL useSingleFlow = [[params objectForKey:@"useSingleFlow"] boolValue];
-      NSString *logo = [params objectForKey:@"logo"];
-
-      if (params[@"customization"]) {
-        NSDictionary *customization = [params objectForKey:@"customization"];
-        companyName = [customization objectForKey:@"companyName"];
-        textColor = [customization objectForKey:@"textColor"];
-        primaryColor = [customization objectForKey:@"primaryColor"];
-      } else {
-        companyName = [params objectForKey:@"companyName"];
-        textColor = [params objectForKey:@"textColor"];
-        primaryColor = [params objectForKey:@"primaryColor"];
-      }
-
-      if ([params objectForKey:@"buttonCorners"]) {
-          buttonCorners = [params objectForKey:@"buttonCorners"];
-      }
-
-      if ([params objectForKey:@"buttonFontSize"]) {
-          buttonFontSize = [params objectForKey:@"buttonFontSize"];
-      }
-
-      if ([params objectForKey:@"buttonPaddings"]) {
-          buttonPaddings = [params objectForKey:@"buttonPaddings"];
-      }
 
       Environment environment = EnvironmentProduction;
       if ([environmentString isEqualToString:@"sandbox"]) {
           environment = EnvironmentSandbox;
       }
+
       if ([environmentString isEqualToString:@"development"]) {
           environment = EnvironmentDevelopment;
       }
+
       if (!self.subscriptionManagerSession) {
-                self.subscriptionManagerSession = [[SubscriptionManagerSession alloc] initWithSessionId:sessionId clientId:clientId environment:environment];
-            }
-      SubscriptionManagerConfiguration *config = [[SubscriptionManagerConfiguration alloc] init];
-      [config setOnSuccessOnSuccess:^(NSString *merchant) {
-          [self sendEventWithName:@"SubscriptionManager-onSuccess" body:@{@"merchant": merchant}];
-      }];
-      [config setOnEventOnEvent:^(NSString * event, NSString * message, NSString * _Nullable taskId) {
+          self.subscriptionManagerSession = [Knot createSubscriptionManagerSessionWithId:sessionId clientId:clientId environment:environment];
+      }
+
+      __weak Knotapi * weakSelf = self;
+
+      self.subscriptionManagerSession.onSuccess = ^(NSString *merchant) {
+          Knotapi * strongSelf = weakSelf;
+          [strongSelf sendEventWithName:@"SubscriptionManager-onSuccess" body:@{@"merchant": merchant}];
+      };
+
+      self.subscriptionManagerSession.onEvent = ^(NSString * event, NSString * message, NSString * _Nullable taskId) {
           NSMutableDictionary *body = [@{@"event": event, @"merchant": message} mutableCopy];
            if (taskId != nil) {
                body[@"taskId"] = taskId;
            }
-          [self sendEventWithName:@"SubscriptionManager-onEvent" body:body];
-      }];
-      [config setOnErrorOnError:^(NSString * error, NSString * message) {
-          [self sendEventWithName:@"SubscriptionManager-onError" body:@{@"errorCode": error, @"errorMessage": message }];
-      }];
-      [config setOnExitOnExit:^{
+          Knotapi * strongSelf = weakSelf;
+          [strongSelf sendEventWithName:@"SubscriptionManager-onEvent" body:body];
+      };
+
+      self.subscriptionManagerSession.onError = ^(NSString * error, NSString * message) {
+          Knotapi * strongSelf = weakSelf;
+          [strongSelf sendEventWithName:@"SubscriptionManager-onError" body:@{@"errorCode": error, @"errorMessage": message }];
+      };
+
+      self.subscriptionManagerSession.onExit = ^{
           [self sendEventWithName:@"SubscriptionManager-onExit" body:nil];
-      }];
-      [config setOnFinishedOnFinished:^{
+      };
+
+      self.subscriptionManagerSession.onFinished = ^{
           [self sendEventWithName:@"SubscriptionManager-onFinished" body:nil];
-      }];
-      [self.subscriptionManagerSession setConfigurationWithConfig:config];
-      [self.subscriptionManagerSession setCompanyNameWithCompanyName:companyName];
+      };
 
-      [self.subscriptionManagerSession setButtonCornersWithButtonCorners:buttonCorners];
-      [self.subscriptionManagerSession setButtonFontSizeWithButtonFontSize:buttonFontSize];
-      [self.subscriptionManagerSession setButtonPaddingsWithButtonPaddings:buttonPaddings];
+      self.subscriptionManagerSession.merchantIds = merchantIds;
+      self.subscriptionManagerSession.merchantNames = merchantNames;
+      self.subscriptionManagerSession.useCategories = useCategories;
+      self.subscriptionManagerSession.useSearch = useSearch;
+      self.subscriptionManagerSession.entryPoint = entryPoint;
 
+      [Knot openWithSession:self.subscriptionManagerSession];
 
-      [self.subscriptionManagerSession setTextColorWithTextColor:textColor];
-      [self.subscriptionManagerSession setPrimaryColorWithPrimaryColor:primaryColor];
-      [self.subscriptionManagerSession setMerchantIdsWithMerchantIds:merchantIds];
-      [self.subscriptionManagerSession setMerchantNamesWithMerchantNames:merchantNames];
-      [self.subscriptionManagerSession setUseSelectionWithUseSelection: useSelection];
-      [self.subscriptionManagerSession setUseCategoriesWithUseCategories: useCategories];
-      [self.subscriptionManagerSession setUseSearchWithUseSearch: useSearch];
-      [self.subscriptionManagerSession setUseSingleFlowWithUseSingleFlow: useSingleFlow];
-      [self.subscriptionManagerSession setLogoWithLogo: logo];
-      [self.subscriptionManagerSession openSubscriptionManagerWithEntryPoint:entryPoint];
-  });
+    });
 }
 
 RCT_EXPORT_METHOD(updateSubscriptionManagerSessionId:(NSString *)sessionId){
